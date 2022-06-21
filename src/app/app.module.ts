9b4c330b88a588184cc1319d5df55766c075de8d
johannpa <johann_paque@hotmail.fr>
2022-06-21 20:16:18 +0200

    make a simple display of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
